19eaac1f11d753e89f69c9960fa07e6dcb347691
Renzo Avila <renzo@avila.ws>
2025-03-22 13:45:03 +0100

    refactor: eliminar componentes UI y constantes no utilizados

--- src/constants/index.ts
+++
@@ -1,7 +0,0 @@
-// Exportar todas las constantes desde un único archivo
-
-export * from './experience';
-export * from './skills';
-export * from './projects';
-export * from './profile';
-export * from './navigation'; 
